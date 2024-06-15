dcbe475f19910a436ece7387bd8f92611f9dc184
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-06-15 18:59:53 +0300

    Added main grass floor and character model

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 400))
+screen = pygame.display.set_mode((800, 550))
 pygame.display.set_caption("Dark Forest")
 clock = pygame.time.Clock()
 
-trees = pygame.image.load('graphics/environment/spooky_trees.png')
-landscape = pygame.image.load('graphics/environment/mountain_landscape.png')
+character = pygame.image.load('graphics/player/char_test/character.png')
+grass = pygame.image.load('graphics/environment/map_grass.png')
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(trees,(200,100))
-    screen.blit(landscape, (0,0))
+  
+    screen.blit(grass, (0,0))
+    screen.blit(character, (0,0))
     
     pygame.display.update()
     clock.tick(60)
