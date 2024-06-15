e102bfd686f26d4de68e25a997d8b6b7d1563f2a
tarsier76 <100511410+tarsier76@users.noreply.github.com>
2024-06-15 16:28:42 +0300

    Updated graphical assets, modified main

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Dark Forest")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+trees = pygame.image.load('graphics/environment/spooky_trees.png')
+landscape = pygame.image.load('graphics/environment/mountain_landscape.png')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface,(200,100))
+    screen.blit(trees,(200,100))
+    screen.blit(landscape, (0,0))
     
     pygame.display.update()
     clock.tick(60)
